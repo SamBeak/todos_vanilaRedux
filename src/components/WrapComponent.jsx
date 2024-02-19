39d60c31f6fb6d07817af9efdf41b45e3557470b
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-02-19 22:08:08 +0900

    docs: Add new Components
    add new Home and Header Component
    set browser router on wrapComponent

--- src/components/WrapComponent.jsx
+++ src/components/WrapComponent.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeComponent from './wrap/HomeComponent';
+import HeaderComponent from './wrap/HeaderComponent';
 
 export default function WrapComponent() {
   return (
-    <div>WrapComponent</div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HeaderComponent />}>
+                <Route index element={<HomeComponent />} />
+                <Route path="/home" element={<HomeComponent />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
   )
 }
