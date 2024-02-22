074746f593d719aa6ba2fb77dd77bf0acc362472
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-02-22 21:48:46 +0900

    feat: Add todo change function
    Add todo functionality to HomeComponent and update styles

--- src/components/wrap/HomeComponent.jsx
+++ src/components/wrap/HomeComponent.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 export default function HomeComponent() {
+  // todo 싱태 관리
+  const [todo, setTodo] = React.useState("");
+  // todo 변경 이벤트
+  const onChangeTodo = (e) => {
+    setTodo(e.target.value);
+    console.log(e.target.value);
+  }
   return (
-    <div>HomeComponent</div>
+    <div id="home">
+      <div className="container">
+        <div className="gap">
+          <div className="title"></div>
+          <div className="content">
+            <div className="todo__container">
+              <label htmlFor="userTodo">What to do ?</label>
+              <input id="userTodo" name="userTodo" type="text" value={todo} onChange={onChangeTodo} autoComplete='off' />
+            </div>
+            <div className="btn__container">
+              <button className="add__btn" type="button">ADD</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
